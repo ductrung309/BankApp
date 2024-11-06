8b84818c1a95410d2eaec5bc89efb71f0a08f83a
trungne <anhlatrung5c@gmail.com>
2024-11-06 21:02:53 +0700

    Add project files.

---
+++ BankApp/Model/BankAccount.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Model
+{
+    public abstract class BankAccount
+    {
+        private string AccountNumber;
+        private decimal Balance;
+        public BankAccount(string accountNumber, decimal balance)
+        {
+            this.AccountNumber = accountNumber;
+            this.Balance = balance;
+        }
+        public string accountNumber
+        {
+            get { return AccountNumber; }
+        }
+        public decimal balance
+        {
+            get { return Balance; }
+            set { if (value >= 0) Balance = value;
+                else throw new ArgumentException("Balance cannot be negative");
+            }
+        }
+        public void Deposit(decimal amount)
+        {
+            if (amount < 0) throw new ArgumentException("Amount cannot be negative");
+            else
+            {
+                balance += amount;
+            }
+        }
+        public virtual void WithDraw(decimal amount)
+        {
+            if (amount < 0) throw new ArgumentException("Amount cannot be negative");
+            else
+            {
+                balance -= amount;
+                Console.WriteLine($"{amount:C} deposited. New balance: {balance:C}");
+            }
+        }
+        public abstract void ApplyMonthlyInterest();
+    }
+}
---
+++ BankApp/Model/SavingAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Model
+{
+    internal class SavingAccount : BankAccount
+    {
+        private decimal InterestRate;
+        public SavingAccount(string AccountNumber, decimal Balance, decimal InterestRate):base(AccountNumber, Balance)
+        {
+            this.InterestRate = InterestRate;
+        }
+        public decimal interestRate
+        {
+            get { return this.InterestRate; }
+            set
+            {
+                if (value >= 0) InterestRate = value;
+                else throw new ArgumentException("Rate cannot be negative");
+            }
+        }
+        public override void ApplyMonthlyInterest()
+        {
+            decimal interest = balance * InterestRate / 100;
+            Deposit(interest);
+            Console.WriteLine($"Monthly interest of {interest:C} applied. New balance: {Balance:C}");
+        }
+    }
+}
